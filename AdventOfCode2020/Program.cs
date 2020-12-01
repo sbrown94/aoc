f1756719635f57992c7783ab26cc771555d77db2
Scott Brown <scott.brown999@gmail.com>
2020-12-01 18:58:09 +0000

    day 1 part 1

---
+++ AdventOfCode2020/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Library;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().Run();
+        }
+
+        private void Run()
+        {
+            var lines = InputHelpers.GetAllLinesAsListInt();
+
+            var combination = GetAnswerCombo(lines);
+
+            Console.WriteLine($"First Number: {combination.firstNo} | Second Number: {combination.secondNo}");
+
+            var answer = combination.firstNo * combination.secondNo;
+
+            Console.WriteLine($"Answer: {answer}");
+        }
+
+        private (int firstNo, int secondNo) GetAnswerCombo(List<int> lines)
+        {
+            for (var i = 0; i < lines.Count; i++)
+            {
+                for (var j = 0; j < lines.Count; j++)
+                {
+                    if (i == j) continue;
+
+                    if (lines[i] + lines[j] == 2020)
+                        return (lines[i], lines[j]);
+                }
+            }
+
+            throw new InvalidOperationException("Combination not found in list");
+        }
+    }
+}
---
+++ lib/Library/InputHelpers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Library
+{
+    public class InputHelpers
+    {
+        public const string DefaultFilePath = @"input.txt";
+
+        public static List<string> GetAllLinesAsListString(string filePath = DefaultFilePath)
+        {
+            return File.ReadAllLines(filePath)?.ToList();
+        }
+
+        public static List<int> GetAllLinesAsListInt(string filePath = DefaultFilePath)
+        {
+            return GetAllLinesAsListString(filePath)?.Select(Int32.Parse).ToList();
+        }
+    }
+}
