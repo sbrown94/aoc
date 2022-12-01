450dfe2a5fdd65601ccc2eadb0d510238c847d44
Scott <scott.brown999@gmail.com>
2022-12-01 23:56:36 +0000

    reshuffle

---
+++ 2021/Day01/Program.cs
@@ -0,0 +1,22 @@
+﻿using Library;
+using System;
+
+namespace TwentyTwo.DayOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().Run();
+        }
+
+        private void Run()
+        {
+            var lines = InputHelpers.GetAllLinesAsListString();
+
+            var answer = GetAnswer(lines);
+
+            Console.WriteLine($"Answer: {answer}");
+        }
+    }
+}
---
+++ Days/Day5/Program.cs
@@ -0,0 +1,41 @@
+﻿using Library;
+using System;
+
+namespace Day5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            new Program().Run();
+        }
+
+        private void Run()
+        {
+            var lines = InputHelpers.GetAllLinesAsListString();
+
+            foreach(var line in lines)
+            {
+                Console.WriteLine($"{ GetBoardingValue(line) }");
+            }
+
+        }
+
+        private int GetBoardingValue(string line)
+        {
+            var range
+
+            for(var i = 0; i < line.Length; i++)
+            {
+                if (line[i] == 'F')
+                    range = range / 2;
+                if (line[i] == 'B')
+                    range = (range / 2) - (range / 4);
+            }
+
+            return range;
+        }
+    }
+}
