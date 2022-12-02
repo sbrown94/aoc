501a28033d3423b05693f01bd023976553faec00
Scott <scott.brown999@gmail.com>
2022-12-02 01:04:18 +0000

    lol

--- 2022/TwentyTwo/Program.cs
+++ 2022/TwentyTwo/Program.cs
@@ -3,14 +3,11 @@ using Microsoft.Extensions.Hosting;
 using TwentyTwo;
 using TwentyTwo.Days;
 
-var builder = Host.CreateDefaultBuilder(args);
-
-builder.ConfigureServices(
-    services =>
-        services
-        .AddSingleton<IWorker, Worker>()
-        .AddSingleton<IDay, DayOne>());
-
-using var host = builder.Build();
-
-host.Services.GetService<IWorker>().Run();
+Host.CreateDefaultBuilder(args)
+    .ConfigureServices(
+        services =>
+            services
+                .AddSingleton<IWorker, Worker>()
+                .AddSingleton<IDay, DayOne>())
+                    .Build()
+                        .Services.GetService<IWorker>().Run();
